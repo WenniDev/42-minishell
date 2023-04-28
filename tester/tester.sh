0eed886cb5a5d493763d56a4c1c1e7be261256b8
Whenever <joachim.padovani@gmail.com>
2023-04-28 18:09:08 +0200

    add tester

--- tester/tester.sh
+++ tester/tester.sh
@@ -22,27 +22,39 @@ function get_diff
 function exec_cmd
 {
   echo "$1" > $INPUT_FILE
-  sleep 1
   if [[ "$2" == "OK" ]]; then
     get_diff "$(tail -1 $LOG_FILE)" "spash: command not found: $1"
   fi
   if [[ "$2" == "KO" ]]; then
     get_diff "$(tail -1 $ERROR_FILE)" "spash: command not found: $1"
   fi
+  echo "Resultat: $(tail -1 $ERROR_FILE)"
+  echo "Excepted: spash: command not found: $1"
 }
 
-rm -f $INPUT_FILE
-rm -f $SAVE_FILE
-mkfifo $INPUT_FILE
-cat > $INPUT_FILE &
-echo $! > $SAVE_FILE
+#rm -f $INPUT_FILE
+#rm -f $SAVE_FILE
+#mkfifo $INPUT_FILE
+#cat > $INPUT_FILE &
+#while :; do cat $INPUT_FILE ; done > /dev/null  &
+#echo $! > $SAVE_FILE
 echo "Minishell init finish"
 
-$MINISHELL 1> $LOG_FILE 2>$ERROR_FILE < $INPUT_FILE &
-echo "Minishell started"
+#$MINISHELL 1>$LOG_FILE 2>$ERROR_FILE < $INPUT_FILE &
+#echo "Minishell started"
 
-exec_cmd "pwd" "KO"
+echo " ---- TEST 1 ---- "
+exec_cmd "efweoihfoiwef" "KO"
+sleep 1
+
+echo " ---- TEST 2 ---- "
+exec_cmd "effwef" "KO"
+sleep 1
+
+echo " ---- TEST 3 ---- "
+exec_cmd "efoiwef" "KO"
+sleep 1
 
 echo "Minishell killed"
 
-rm -rf $INPUT_FILE $SAVE_FILE
+#rm -rf $INPUT_FILE $SAVE_FILE
