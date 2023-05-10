7557a414fff1fe5982dc70752397f1e3e2a08d55
Haoov <rpasabb@gmail.com>
2023-05-10 17:25:42 +0200

    no expand cmd name

--- sources/parser/actions.c
+++ sources/parser/actions.c
@@ -80,6 +80,7 @@ void	add_word(t_parser *p, t_word_d wd)
 	}
 	else
 	{
+		new_wl->word->flags |= W_NOEXPAND;
 		p->cl_curr->cmd.elem.words = new_wl;
 		if (ft_strstr(BUILTINS, new_wl->word->lval))
 			p->cl_curr->cmd.flags |= CMD_BUILTIN;
