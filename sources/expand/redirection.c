92b06593c296ec67cfac583e69d12ad3fc12e808
Whenever <joachim.padovani@gmail.com>
2023-05-16 19:44:35 +0200

    fix redirect expand

--- sources/expand/redirection.c
+++ sources/expand/redirection.c
@@ -6,7 +6,7 @@
 /*   By: jopadova <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/15 15:49:01 by jopadova          #+#    #+#             */
-/*   Updated: 2023/05/15 18:33:08 by jopadova         ###   ########.fr       */
+/*   Updated: 2023/05/16 17:53:51 by jopadova         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,32 +15,42 @@
 #include "signal.h"
 #include "libft.h"
 
-void	clone_word(t_word_d *dest, t_word_d *src)
+t_word_d 	*clone_word(t_word_d *src)
 {
-	dest = sfcalloc(1, sizeof(t_word_d));
-	dest->lval = ft_strdup(src->lval);
-	dest->flags = src->flags;
+	t_word_d	*tmp_word;
+
+	tmp_word = sfcalloc(1, sizeof(t_word_d));
+	tmp_word->lval = ft_strdup(src->lval);
+	if (!tmp_word->lval)
+		return (free(tmp_word), kill(0, SIGABRT), (void *) NULL);
+	tmp_word->flags = src->flags;
+	return (tmp_word);
 }
 
-t_red	*expand_red(t_red *r)
+void	*expand_red(t_word_d *filename)
 {
 	int			status;
 	t_word_lst	*tmp_lst;
 	t_word_d	*tmp_word;
 
-	clone_word(tmp_word, r->filename);
+	status = 0;
+	tmp_word = clone_word(filename);
 	if (tmp_word->flags & W_DOLLAR)
 		expand_env(tmp_word, &status);
 	if (status)
-		(free_word_lst(tmp_lst), kill(0, SIGABRT));
+		(free_word(tmp_word), kill(0, SIGABRT));
 	tmp_lst = ft_calloc(1, sizeof(t_word_lst));
 	if (!tmp_lst)
 		(free_word(tmp_word), kill(0, SIGABRT));
 	tmp_lst->word = tmp_word;
-	expand_wildcard(&tmp_lst, &status);
+	expand_wildcard(&tmp_lst, &tmp_lst, &status);
 	if (status)
 		(free_word_lst(tmp_lst), kill(0, SIGABRT));
 	if (ft_strchr(tmp_lst->word->lval, '*') || tmp_lst->next)
 		return (free_word_lst(tmp_lst), NULL);
-	return (free_word_lst(tmp_lst), r);
+	free(filename->lval);
+	filename->lval = ft_strdup(tmp_lst->word->lval);
+	if (!filename->lval)
+		(free_word_lst(tmp_lst), kill(0, SIGABRT));
+	return (free_word_lst(tmp_lst), filename);
 }
