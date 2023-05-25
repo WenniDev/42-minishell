5e7d4310f9182473db055b05871321c7ab0ec52b
Haoov <rpasabb@gmail.com>
2023-05-25 15:16:00 +0200

    heredoc print fixed

--- srcs/signal_handler.c
+++ srcs/signal_handler.c
@@ -3,13 +3,6 @@
 
 t_data	*g_msh;
 
-void	handle_sigabrt(int signum)
-{
-	(void)signum;
-	clean_all(g_msh);
-	exit(EXS_SIGSEG);
-}
-
 void	redisplay(int signum)
 {
 	if (signum == SIGINT)
@@ -22,33 +15,52 @@ void	redisplay(int signum)
 	rl_redisplay();
 }
 
-void	handle_sig(int signum)
+void	handle_sigabrt(int signum)
 {
-	if (signum == SIGINT && g_msh->parser.state & PST_HEREDOC)
-		close(0);
+	(void)signum;
+	clean_all(g_msh);
+	exit(EXS_SIGSEG);
+}
+
+void	handle_sigint(int signum)
+{
+	if (g_msh->parser.state & PST_HEREDOC)
+	{
+		printf("\n");
+		close(STDIN_FILENO);
+	}
 	if (g_msh->exec.child_nb)
 	{
 		if (g_msh->exec.child == false)
 			signal(SIGTERM, SIG_IGN);
-		if (signum == SIGQUIT)
-				printf("%s", QUITMSG);
+		printf("\n");
+		kill(0, SIGTERM);
+	}
+	else if (!(g_msh->parser.state & PST_HEREDOC))
+		redisplay(signum);
+	g_msh->status = 130;
+	g_msh->parser.state &= ~PST_HEREDOC;
+}
+
+void	handle_sigquit(int signum)
+{
+	if (g_msh->exec.child_nb)
+	{
+		if (g_msh->exec.child == false)
+			signal(SIGTERM, SIG_IGN);
+		printf("%s", QUITMSG);
 		printf("\n");
 		kill(0, SIGTERM);
 	}
 	else
 		redisplay(signum);
-	if (signum == SIGINT)
-		g_msh->status = 130;
-	if (signum == SIGQUIT)
-		g_msh->status = 131;
-	if (signum == SIGINT)
-		g_msh->parser.state &= ~PST_HEREDOC;
+	g_msh->status = 131;
 }
 
 void	signal_handler(t_data *msh)
 {
 	g_msh = msh;
 	signal(SIGABRT, handle_sigabrt);
-	signal(SIGINT, handle_sig);
-	signal(SIGQUIT, handle_sig);
+	signal(SIGINT, handle_sigint);
+	signal(SIGQUIT, handle_sigquit);
 }
